35b98997b52e16258dc45bd863ce911925c4cd35
Benjamin Flin <brf2117@columbia.edu>
2021-03-31 21:56:34 -0700

    modified closed ast and added example

--- lib/closure/cast.ml
+++ lib/closure/cast.ml
@@ -5,8 +5,8 @@ type cty
   = CIntT
   | CBoolT
   | CharT
-  | CArr of cty * cty
   | CDataTy of cname 
+  | CClosT of cname 
 
 type cexpr
   = CInt of int
@@ -17,12 +17,12 @@ type cexpr
   | CArg of cindex * cty 
   | CIf  of cexpr * cexpr * cexpr * cty
   | CConstruction of cname * cexpr list * cty
-  | CCase of ccase_alt list
+  | CCase of cexpr * ccase_alt list
 and ccase_alt 
-  = CDestructor of cname * cexpr * cty
+  = CDestructor of cname * cty list * cexpr * cty
   | CWildcard of cexpr * cty
 
-type ccons = cty list
+type ccons = cname * cty list
 type cglobaldef = cname * cty list * cexpr * cty
 type cdataty = cname * ccons list
 
@@ -31,3 +31,37 @@ type program = {
   datatys : cdataty list;
   main : cexpr;
 }
+
+(*
+let prog : program = {
+  datatys = [
+    "List_Int", ["Cons", [CIntT, CDataTy "List_Int"]; "Nothing", [] ] 
+  ]
+  globals = [
+    "foldl", 
+      [CClosT "f"], 
+      CClos ("foldl1", [CArg 0]),
+      CClosT "foldl1";
+    "foldl1", 
+      [CClosT "f"; CInt], 
+      CClos ("foldl2", [CArg 0, CArg 1]),
+      CClosT "foldl2";
+    "foldl2", [CClosT "f"; CInt; CDataTy "List_Int"], 
+      CCase (CArg 2, [
+      CDestructor ("Nil", [], CArg 1); 
+      CDestructor ("Cons", [IntT; CDataTy "List_Int"], 
+        CApp(
+          CApp(
+            CApp(
+              CClos("foldl", []), CArg 0
+            ), 
+          CApp(
+            CApp(CArg 0, CArg 1)
+          , CArg 3)
+          ), CArg 4))])
+    ]
+  main = 
+  CApp(CApp(CApp(CClos "foldl", CClos "f"), CInt 0), 
+  CConstruction ("Cons", [CInt 0, CConstruction ("Cons", [CInt 1, CConstruction "Nil" []])]))
+}
+*) 
