440617c1e9ab0880af0d06ac8117feebb4cbfe8e
Jay Karp <jaykarp2@gmail.com>
2021-04-24 11:52:27 -0500

    updated hook2

--- test.sh
+++ test.sh
@@ -5,4 +5,7 @@ docker-compose run lingo_testbed bash -c "python3 ./reg-tests/RunTests.py $ARGS"
 FAIL=$?
 # Clean Up Stopped Docker Container
 docker rm $(docker ps -a -q) > /dev/null 2>&1
-[ $FAIL -eq 1 ] && exit 1
+if [[ $FAIL -eq 1 ]]
+then 
+    exit 1
+fi
